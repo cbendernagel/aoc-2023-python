6bcc694a40255411130510385c883772f3619f85
cbenderngel <cbendernagel@gmail.com>
2024-04-17 21:44:03 -0400

    part1 - complete

--- day1/part1.py
+++ day1/part1.py
@@ -1,9 +1,12 @@
 def main():
     input = open('./day1/input.txt', 'r')
+    sum = 0
     for line in input:
-        print(line)
-    print("hello world")
-    
+        nums = [int(i) for i in line if i.isdigit()]
+        temp = nums[0] * 10 + nums[len(nums) - 1]
+        sum += temp
+        
+    print(sum)
     
 if __name__ == '__main__':
     main()
